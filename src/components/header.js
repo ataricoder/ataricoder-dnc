42cc8a141732e1c2793e324d47e3fbe3125a9443
ataricoder <ataricoder@gmail.com>
2019-11-26 13:21:46 -0600

    finished applying stateful event to the header along its css

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,48 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 import "./Header.css"
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/">
-        <img src={require("../images/logo-designcode.svg")} width="30" />
-      </Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy">
-        <button>Buy</button>
-      </Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+    this.state = {
+      hasScrolled: false,
+    }
+  }
 
-Header.defaultProps = {
-  siteTitle: ``,
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className={this.state.hasScrolled ? "Header HeaderScrolled" : "Header"}
+      >
+        <div className="HeaderGroup">
+          <Link to="/">
+            <img src={require("../images/logo-designcode.svg")} width="30" />
+          </Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy">
+            <button>Buy</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header
