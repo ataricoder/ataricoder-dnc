e1734910be305980efec7ee87869ae634bc12bf4
ataricoder <ataricoder@gmail.com>
2019-11-26 12:00:58 -0600

    updated cards and added them to index page, did some style changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,11 +17,28 @@ const IndexPage = () => (
     </div>
     <div className="Cards">
       <h2>11 courses, more coming</h2>
-      <Card
-        title="React for Designers"
-        text="12 sections"
-        image={require("../images/wallpaper.jpg")}
-      />
+      <div className="CardGroup">
+        <Card
+          title="Design Systems"
+          text="10 sections"
+          image={require("../images/wallpaper.jpg")}
+        />
+        <Card
+          title="React for Designers"
+          text="12 sections"
+          image={require("../images/wallpaper2.jpg")}
+        />
+        <Card
+          title="Sound Design"
+          text="5 sections"
+          image={require("../images/wallpaper3.jpg")}
+        />
+        <Card
+          title="ARKit 2"
+          text="10 sections"
+          image={require("../images/wallpaper4.jpg")}
+        />
+      </div>
     </div>
     <SEO title="Home" />
   </Layout>
